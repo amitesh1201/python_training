f510aaf6eb004bb12b940a067b9981c77adfa15f
swapnilz30 <swapnilzarbade@gmail.com>
2021-07-18 07:48:20 +0000

    foor_loop2.py foor_loop3.py

--- foor_loop2.py
+++ foor_loop2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+print ("========List========")
 # List
 words = ['one', 'two', 'three']
 
@@ -7,3 +8,21 @@ print (words)
 
 for w in words:
     print (w)
+
+print ("========Tuple========")
+# Tuple
+tuple1 = ("one", "two", "three")
+print (tuple1)
+tuple2 = (1,2,3,4,5)
+print (tuple2)
+
+print ("")
+
+for t1 in tuple1:
+    print (t1)
+
+
+print ("")
+
+for t2 in tuple2:
+    print (t2)
---
+++ foor_loop3.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+# Declaration of list
+list1 = ["one", "two", "three", "four", "five"]
+print (list1)
+print ("")
+
+# Replace the value in 2nd postision in list
+list1[2] = 10
+
+print (list1)
+
+print("")
+
+# Added a list in 1st position of list1
+list1 [1] = [22, 33, 44]
+
+print (list1)
+
+# Print the value of 2nd position 
+word = "Python"
+
+print (word[2])
+
