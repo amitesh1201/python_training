9c2143b5745534c77289c069caec62c4918f47bf
root <swapnilzarbade@gmail.com>
2021-07-22 04:11:42 +0000

    Added for_loop4_nested_list.py

--- for_loop4_nested_list.py
+++ for_loop4_nested_list.py
@@ -4,6 +4,8 @@
 list2 = ["one",[1,2,3], "two", "three", "four", "five"]
 
 print ("Nested list: ", list2)
+print ("The postion of list2[0][0]: ", list2[0][0])
+print ("The postion of list2[0][1]: ", list2[0][1])
 print ("The postion of list2[1]: ", list2[1])
 print ("The postion of list2[1][0]: ", list2[1][0])
 print ("The postion of list2[1][1]: ", list2[1][1])
