7dcdd009ec45645bd5680007ca94c67c586057e6
root <swapnilzarbade@gmail.com>
2021-07-23 04:54:06 +0000

    Added list_pop.py

---
+++ list_pop.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+
+
+list1 = [1,2,3,4,5]
+
+print ("The original list: ", list1)
+
+print ("")
+
+# The pop function remove the last element in the list
+list1.pop()
+print ("Removed last element: ", list1)
+
+print ("")
+# The pop function remove element in 1st position in the list
+list1.pop(1)
+print ("Removed element in  1st position: ", list1) 
