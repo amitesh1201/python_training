efe35c8e98367d90e87929dc4d2b8c4ca5110f06
root <swapnilzarbade@gmail.com>
2021-07-23 05:29:51 +0000

    modified:   list_remove_pop.py

--- list_remove_pop.py
+++ list_remove_pop.py
@@ -7,7 +7,11 @@ list1 = [1,2,3,4,5]
 print ("The original list: ", list1)
 
 print ("")
+list1.remove(2)
+print ("Remove element in the list: ", list1)
+print ("Length of list: ", len(list1))
 
+print ("")
 # The pop function remove the last element in the list
 list1.pop()
 print ("Removed last element: ", list1)
