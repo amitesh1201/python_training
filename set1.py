1fdee3998ba5ab15d182421b54f9f32b03ec157f
root <swapnilzarbade@gmail.com>
2021-07-28 14:27:56 +0000

    added set1.py

---
+++ set1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+# Collection of multiple items
+# Unordered and unindexed
+# Items are unique in the set
+
+set1 = set((1,2,3))
+print ("set1: ", set1)
+
+set_list = set(['a','b','c'])
+print ("set_list: ", set_list)
+
+set2 = set((1,'a',2,'c',3,4,4))
+print ("set2: ", set2)
