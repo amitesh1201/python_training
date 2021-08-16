cabffbade3fce1b79df2efac7cf1f64b7f2f38ac
root <swapnilzarbade@gmail.com>
2021-08-16 15:28:05 +0000

    Added module_os.py

---
+++ module_os.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import os
+
+#
+# os.listdir("directory_path") require input as directory path. 
+# This function display list of files and directories in the specifies path.
+#
+print (os.listdir("/"))
+
+create_dir = "test-dir"
+
+# The os.makedirs("direcotry_name") function creats the directory
+os_create_dir = os.makedirs(create_dir)
+
+print (os_create_dir)
+print ("Checks the direcory exists or not (True/False): ", os.path.isdir(create_dir))
+print ("Checks the /python_training/one.py file exist or not (True/False): ", os.path.isfile("/python_training/one.py"))
+# The os.getcwd() function displays the current working directory
+print (os.getcwd())
+
+# The os.chdir("existing_direcotry_name") function change director. 
+os.chdir("/")
+print (os.getcwd())
