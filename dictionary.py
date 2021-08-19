6408bc165babe3adbca756e99c13d60d7fce7401
swapnilz30 <swapnilzarbade@gmail.com>
2021-08-19 04:40:18 +0000

    modified:   dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -25,3 +25,15 @@ print ("\ndict1.get(3): ", dict1.get(3))
 # Such types of conditions handled in dynamic programing
 #
 print ("\ndict1.get(4): ", dict1.get(4))
+
+# The keys() function displays the all keys in the form of list in the dicntionary.
+print ("\nThe keys() function displays the all keys in the form of list in the dicntionary.")
+print ("dict1.keys(): ", dict1.keys())
+
+# The values() function displays the all values of keys in the form of list  in the dicntionary.
+print ("\nThe values() function displays the all values of keys in the form of list  in the dicntionary.")
+print ("dict1.values(): ", dict1.values())
+
+# The items() function deplays the disctionary itmes in form of tuples in list.
+print ("\nThe items() function deplays the disctionary itmes in form of tuples in list.")
+print ("dict1.items(): ",dict1.items())
