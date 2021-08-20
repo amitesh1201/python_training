d6364bfa21faf200a5f2304e03002629eb126b5c
swapnilz30 <swapnilzarbade@gmail.com>
2021-08-20 04:21:08 +0000

    modified:   dictionary_add_update_del.py

--- dictionary_add_update_del.py
+++ dictionary_add_update_del.py
@@ -3,15 +3,25 @@
 dict1 = {1: 'A', 2: 'B', 3: 'C'}
 
 dict1[4] = 'E'
+print ("Add the key pair:\n")
 print ("dict1[4] = 'E':\n")
 print (dict1)
 
 print ("\n================\n")
 dict1[3] = 'F'
+print ("Replace the value:\n")
 print ("dict1[3] = 'F':\n")
 print (dict1)
 
 print ("\n================\n")
 del dict1[2]
+print ("Delete the key pair:\n")
 print ("del dict1[2]\n")
 print (dict1)
+
+
+print ("\n================\n")
+print ("Empty dict1 dictionary:\n")
+dict1.clear()
+print ("dict1.clear(): ", dict1)
+
