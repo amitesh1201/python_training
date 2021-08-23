1ae2a90b531567816f402c8fb2ff0e9ac9791afe
swapnilz30 <swapnilzarbade@gmail.com>
2021-08-23 14:02:57 +0000

    new file:   function.py

---
+++ function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#
+# The function is the collection of statements.
+# Instead of repeating the same code/statements in the program,
+# we can create the set of code/statements assign the name to the set call function. \
+# The function reduces the repeated code structure and quickly calls whenever required. 
+# There are types of functions.
+#    1. Nonaurgument function: In such functions do not require argument.
+#    2. Single or multi-argument function: The functions require single or multiple arguments according to function structure.
+# In any programing language, the user can define the function and use the programmer to define the function.
+#
