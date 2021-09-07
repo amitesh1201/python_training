9d64493e89a68b357d161af371e7f5f0ac38f161
swapnilz30 <swapnilzarbade@gmail.com>
2021-09-07 02:38:48 +0000

    modified:   file_search.py

--- file_search.py
+++ file_search.py
@@ -8,8 +8,13 @@ file_state = open(file_name,"rt")
 
 data = json.load(file_state)
 
-print (data['book'])
-#for r in data:
-#   #if [ data.keys() == 777  ]:
-#   #     print (r)
-#   print (r.keys())
+print ("len(data['book']): ",len(data['book']))
+print ("\ndata['book'][0]: \n", data['book'][0])
+print ("\ndata['book'][1]: \n",data['book'][1])
+
+
+print (data['book'][0]['id'])
+#for r in len(data['book']):
+#   if [ data.keys() == 777  ]:
+#        print (r)
+#   print (r)
