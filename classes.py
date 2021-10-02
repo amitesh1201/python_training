e0d43ee057327f9862df51a3bea91aa9d6ca8bdc
swapnilz30 <swapnilzarbade@gmail.com>
2021-10-02 04:52:16 +0000

    Added classes.py

---
+++ classes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+class Name:
+
+    # The __init__ function initialize complete class OR constructor 
+    def __init__(self):
+        print(self, "This is __init__ function with reference self")
+
+# initialize the object
+n1 = Name ()
+# Print the reference of the class
+print (n1) 
